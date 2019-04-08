5a7ba4575c467c6b501bf7b84b20d63b1b5d06d2
Sridhar Krishnan <sridhar.krishnan@realisticgames.co.uk>
2019-04-08 15:23:25 +0100

    working project - local / remote

--- hello.js
+++ hello.js
@@ -13,37 +13,52 @@ var http = require("http");
 // to pull the content from external website
 var request = require("request"); // npm install request --save
 
-var reqCounter  = 0;
-var bb = null;
-
 // Create a quick web server in nodejs
 http.createServer(function(req, res)
 {
-	bb = res;
+
 	// Using request(kind of curl) pull the content from 
 	// blockchain.info website
 	request({
 			url:"https://blockchain.info/stats?format=json",
 			json: true
 		}, function(error, response, body){
+
 		// print the pulled body content
-		console.log(body);
-		//console.log(response);
-		//response.write(body); //write a response to the client
-		//res.end(); //end the response
-		//response.end(body);
-		bb.end(JSON.stringify(body));
+		//console.log(body);
+		res.end(JSON.stringify(body));
 	});
 
-	// server log
-	//console.log(++reqCounter + ' Request from => ' + req.url);
-	
+	/*
+	// 1. Run locally
 	// Run the webserver => node createWebserver.js
-	// Request the webserver from browser => localhost:99
-	// Prints the welcome msg in the browser
-	//res.end('Hello ... new visitor...sridhar');
-// }).listen(1234);
-}).listen(process.env.PORT ||80); //the server object listens on port 8080
+	// Request the webserver from browser => localhost:1234
+
+	//2. Run remotely - Heroku
+	//-----------------------
+	1. mkdir demo -> cd demo -> npm init -fy (creats package.json)
+	2. npm i request --save (add dependecy(request module details) to package.json)
+	3. Inside package.json, configure the start program inside "scripts" 
+	"scripts": {
+		"start": "node hello.js"
+	  },
+
+	4. commit the changes to 'your' git repo.
+	----------------------------------------------
+	5. heroku login 
+	6. heroku create
+	7. git push heroku master (push the project to heroku for deployment)
+	8. heroku open
+
+	// ref: https://medium.com/@grantspilsbury/build-and-deploy-a-node-express-server-to-heroku-in-10-steps-70c936ab15dc
+
+	*/
+
+
+
+	// process.env.PORT  - Heroku
+	// 1234 - local server
+}).listen(process.env.PORT ||1234); //the server object listens on port 8080
 
 /*
 OUTPUT:
